0bb396748e2a81359dbdb8e209d685f0828632de
Kaptah <quirumedik@gmail.com>
2023-03-11 09:23:50 -0600

    rellena select de cripto de manera dinamica

--- dist/js/cryptos.js
+++ dist/js/cryptos.js
@@ -2,17 +2,35 @@ const btnCryp = document.querySelector('#btnCryp');
 const temp = document.querySelector('#temp');
 const cryp = document.querySelector('#cryp');
  const url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,MXN,EUR';
-
+// Crear un Promise que devuelve las criptomonedas
+const obtenerCriptomonedas  = criptomonedas => new Promise( resolve => {
+    resolve(criptomonedas);
+});
 document.addEventListener('DOMContentLoaded', () => {
-	btnCryp.addEventListener('click', (e) =>{
-        //e.preventDefault();
-        //const temporalidad = temp.value;
-        //console.log(temporalidad);
-       
+	consultarCriptomonedas();
+    btnCryp.addEventListener('click', (e) =>{
+              
         getCrypto();
     }) 
 });
-
+//Rellenar select de criptomonedas
+function consultarCriptomonedas() {
+    const urlcryp = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=15&tsym=USD';
+    fetch(urlcryp)
+        .then( respuesta => respuesta.json() ) 
+        .then( resultado => obtenerCriptomonedas(resultado.Data))
+        .then( criptomonedas => selectCriptomonedas(criptomonedas) )
+}
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach( cripto => {
+        const { FullName, Name } = cripto.CoinInfo;
+        const option = document.createElement('option');
+        option.value = Name;
+        option.textContent = FullName;
+        cryp.appendChild(option);
+    })
+}
+//Fin rellenar select
 const getCrypto = async () => {
     const response = await fetch(url); // GET
     const json = await response.json();
