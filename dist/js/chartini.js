91d86c03cd5938554513885be67c56a77f2717bb
Kaptah <quirumedik@gmail.com>
2023-03-15 18:45:31 -0600

    Modificacion de como va quedar el grafico

--- dist/js/chartini.js
+++ dist/js/chartini.js
@@ -6,34 +6,22 @@ new Chart(document.getElementById("line-chart"), {
       datasets: [
         {
           data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
-          label: "Africa",
+          label: "Peso",
           borderColor: "#3e95cd",
           fill: false,
         },
         {
           data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
-          label: "Asia",
+          label: "Euro",
           borderColor: "#36a2eb",
           fill: false,
         },
         {
           data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
-          label: "Europe",
+          label: "Dolar",
           borderColor: "#07b107",
           fill: false,
-        },
-        {
-          data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
-          label: "Latin America",
-          borderColor: "#ff6384",
-          fill: false,
-        },
-        {
-          data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
-          label: "North America",
-          borderColor: "#4bc0c0",
-          fill: false,
-        },
+        },        
       ],
     },
     options: {
