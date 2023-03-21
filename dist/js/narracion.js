6ebb8826b5b3abe7976bcf987f54f8e68542d93a
Jesus-007-cmd <jesusgtz1@hotmail.com>
2023-03-20 18:45:57 -0600

    se agrega un audio de bienvenida al entrar al programa

---
+++ dist/js/narracion.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //11111111111  En esta sección da el mensaje de bienvenida de nuestra aplicación 11111111111111111
+        var voz;
+        voz = window.speechSynthesis.getVoices();
+        var confvoz = new SpeechSynthesisUtterance();
+        confvoz.rate = 1;
+        confvoz.pitch = 0.5;
+        confvoz.lang = 'es-MX';
+        confvoz.text = 'Bienvenido a Nuestra aplicación de API';
+        confvoz.voice = voz[0];
+        window.speechSynthesis.speak(confvoz);
+    //11111111111Aqui finaliza la parte del mensaje de bienvenida de voz de nuestra aplicación11111111111111111
+        })
